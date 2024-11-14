665a17636a51b3d652792a3aac71b6696af830fe
guotuluo <2403105429@qq.com>
2024-11-14 17:41:23 +0800

    develop: 开发全局返回异常实体

---
+++ BackEnd/src/main/java/com/haitu/prototype/common/convention/exception/ClientException.java
@@ -0,0 +1,34 @@
+package com.haitu.prototype.common.convention.exception;
+
+import com.haitu.prototype.common.convention.errorcode.BaseErrorCode;
+import com.haitu.prototype.common.convention.errorcode.IErrorCode;
+
+/**
+ * 客户端异常
+ */
+public class ClientException extends AbstractException {
+
+    public ClientException(IErrorCode errorCode) {
+        this(null, null, errorCode);
+    }
+
+    public ClientException(String message) {
+        this(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
---
+++ BackEnd/src/main/java/com/haitu/prototype/common/convention/exception/RemoteException.java
@@ -0,0 +1,31 @@
+package com.haitu.prototype.common.convention.exception;
+
+
+import com.haitu.prototype.common.convention.errorcode.BaseErrorCode;
+import com.haitu.prototype.common.convention.errorcode.IErrorCode;
+
+/**
+ * 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
