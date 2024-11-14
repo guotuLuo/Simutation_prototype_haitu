b1cbd7e43ff7c97f6f6d8b505db07303de3bf917
guotuluo <2403105429@qq.com>
2024-11-14 17:40:36 +0800

    develop: 跨域请求问题，允许来自不同前端端口的跨域请求

--- BackEnd/src/main/java/com/haitu/prototype/configuration/WebConfig.java
+++ BackEnd/src/main/java/com/haitu/prototype/configuration/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:63342")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
