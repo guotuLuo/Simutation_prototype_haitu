7ec330865bf5b105502df16fd84e1d5088332f25
guotuluo <2403105429@qq.com>
2024-11-14 17:38:16 +0800

    develop: 点迹实体类

--- BackEnd/src/main/java/com/haitu/prototype/dao/entity/Point.java
+++ BackEnd/src/main/java/com/haitu/prototype/dao/entity/Point.java
@@ -1,11 +1,15 @@
 package com.haitu.prototype.dao.entity;
-
-
+import lombok.Data;
 import lombok.Getter;
 
+@Data
 @Getter
 public class Point {
     public String lat;
     public String lon;
 
+    public Point(String lat, String lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
 }
