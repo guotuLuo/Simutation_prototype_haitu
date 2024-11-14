3396ce79ae9a57c711c1b5187d8029b2cc4e7906
guotuluo <2403105429@qq.com>
2024-11-14 17:39:50 +0800

    develop: 开发雷达前端获取所有飞机最新点迹的功能

--- BackEnd/src/main/java/com/haitu/prototype/controller/RadarController.java
+++ BackEnd/src/main/java/com/haitu/prototype/controller/RadarController.java
@@ -1,32 +1,41 @@
 package com.haitu.prototype.controller;
 
 import com.haitu.prototype.common.convention.result.Result;
+import com.haitu.prototype.common.convention.result.Results;
 import com.haitu.prototype.dao.entity.Airplane;
-import com.haitu.prototype.dao.entity.Radar;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.haitu.prototype.dao.entity.Point;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/radars")
 public class RadarController {
-
-    @Autowired
-    private HashMap<String, Airplane> airplaneHashMap;
-
-    @Autowired
-    private HashMap<String, Radar> radarHashMap;
+    private final HashMap<String, List<Point>> pointList;
 
     @GetMapping ("/scan")
-    public Result<HashMap<String, Airplane>> coordinatesForRadar() {
+    public Result<List<Airplane>> coordinatesForRadar() {
         // 返回整个 HashMap，Spring 会自动将其序列化为 JSON 格式
-        return new Result<HashMap<String, Airplane>>().setData(airplaneHashMap);
+        List<Airplane> list = new ArrayList<>();
+        for (String s : pointList.keySet()) {
+            List<Point> points = pointList.get(s);
+            if(!points.isEmpty()){
+                Airplane airplane = new Airplane(s, points.get(points.size() - 1).lat, points.get(points.size() - 1).lon);
+                list.add(airplane);
+            }
+        }
+        return Results.success(list);
     }
 
     @PostMapping("/delete")
     public void delete(@RequestParam String uuid){
         System.out.println("删除雷达： " + uuid);
-        radarHashMap.remove(uuid);
+        pointList.remove(uuid);
     }
 }
