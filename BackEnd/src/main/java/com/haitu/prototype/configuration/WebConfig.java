48fc6cb645157cc71d54bffb29c3cb201e477808
guotuluo <2403105429@qq.com>
2024-11-19 16:27:10 +0800

    optimize: 添加跨域请求端口63342，放弃vscode默认端口5500，因为5500会热加载

--- BackEnd/src/main/java/com/haitu/prototype/configuration/WebConfig.java
+++ BackEnd/src/main/java/com/haitu/prototype/configuration/WebConfig.java
@@ -13,7 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:63342")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
