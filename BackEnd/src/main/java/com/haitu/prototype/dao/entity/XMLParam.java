7c17ff3ac70cea566ef2908ccc8b64cab70777af
guotuluo <2403105429@qq.com>
2024-11-22 22:16:29 +0800

    develop: xml解包自动装配类

---
+++ BackEnd/src/main/java/com/haitu/prototype/dao/entity/XMLParam.java
@@ -0,0 +1,25 @@
+package com.haitu.prototype.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class XMLParam {
+    /**
+     * 参数名称
+     * */
+    private String name;
+    /**
+     * 参数类型
+     * */
+    private String type;
+    /**
+     * 相对偏移量
+     * */
+    private int offset;
+    /**
+     * 放大倍数
+     * */
+    private int resolution;
+}
