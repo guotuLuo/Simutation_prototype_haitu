6980d178389783f2f1a8944cd7c7a2b2b61ef2e8
guotuluo <2403105429@qq.com>
2024-11-22 22:16:09 +0800

    develop: 点迹请求实体修改

--- BackEnd/src/main/java/com/haitu/prototype/dto/request/PointReqDTO.java
+++ BackEnd/src/main/java/com/haitu/prototype/dto/request/PointReqDTO.java
@@ -3,8 +3,17 @@ package com.haitu.prototype.dto.request;
 import lombok.Getter;
 
 @Getter
-public class PointRequest {
+public class PointReqDTO {
+    /**
+     * 点击id
+     * */
     public String id;
+    /**
+     * 点迹维度
+     * */
     public String lat;
+    /**
+     * 点迹经度
+     * */
     public String lon;
 }
