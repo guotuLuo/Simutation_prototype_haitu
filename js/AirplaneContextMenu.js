6cce797b6ff252b12dcc7105e8b7f343e73f7cc6
guotuluo <2403105429@qq.com>
2024-11-26 16:45:23 +0800

    develop: 修改前端飞机类发送请求的方式，尽量改成axios异步

--- js/AirplaneContextMenu.js
+++ js/AirplaneContextMenu.js
@@ -7,10 +7,10 @@ class AirplaneContextMenu extends ContextMenu {
     initializeMenu() {
         // 待修改得固定飞行路线
         // const routeCoordinates = [[25, 118], [25, 119], [25, 120], [25, 121], [25, 122], [25, 123]];
-        
+
         const menuList = this.menu.querySelector("ul");
         menuList.innerHTML = ""; // 清空菜单
-        
+
 
         const setRoutesItem = document.createElement("li");
         setRoutesItem.textContent = "设置点迹";
@@ -52,5 +52,35 @@ class AirplaneContextMenu extends ContextMenu {
             }
         };
         menuList.appendChild(deleteItem);
+
+        const changeNameItem = document.createElement("li");
+        changeNameItem.textContent = "修改名称";
+        changeNameItem.onclick = () => {
+            if (this.currentItem) {
+                this.currentItem.changeName();
+                this.hide();
+            }
+        };
+        menuList.appendChild(changeNameItem);
+
+        const stopFlightItem = document.createElement("li");
+        stopFlightItem.textContent = "停止飞行";
+        stopFlightItem.onclick = () => {
+            if (this.currentItem) {
+                this.currentItem.stopFlight();
+                this.hide();
+            }
+        };
+        menuList.appendChild(stopFlightItem);
+
+        const deleteRouteItem = document.createElement("li");
+        deleteRouteItem.textContent = "删除点迹";
+        deleteRouteItem.onclick = () => {
+            if (this.currentItem) {
+                this.currentItem.deleteRoute();
+                this.hide();
+            }
+        };
+        menuList.appendChild(deleteRouteItem);
     }
 }
