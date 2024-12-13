84ddfcdd2f6d64b37021ea7762eb783d87418327
guotuluo <2403105429@qq.com>
2024-12-13 16:43:09 +0800

    refactor: 修复飞机点迹删除和初始坐标设置问题

--- js/components/reconnaissance.js
+++ js/components/reconnaissance.js
@@ -1,11 +1,11 @@
 // Jamming 类
-class Reconnaissance {
+class Reconnaissance extends BaseComponent{
     constructor(map, position, icon, contextMenu, itemType, className, name) {
+        super();
         this.map = map;
         this.position = position;
         this.icon = icon;
         this.contextMenu = contextMenu;
-        this.routes = [];
         this.routeMarkers = [];
         this.speed = 300;
         this.moving = false;
@@ -20,6 +20,14 @@ class Reconnaissance {
         this.latitude = position.lat;
         this.longitude = position.lng;
         this.altitude = 0;
+
+        this.batch = parseInt(this.name.substring(this.itemType.length));
+        this.use = 2;
+        // 将纬度和经度格式化为字符串
+        this.track = [];
+        const lat = position.lat.toFixed(5);
+        const lng = position.lng.toFixed(5);
+        this.track.push([lat, lng]);
     }
     backToStart(){
         this.marker.setLatLng(this.startposition);
@@ -30,17 +38,35 @@ class Reconnaissance {
     createMarker() {
         this.id = generateUUID();
         this.marker = L.marker(this.position, {icon: this.icon, draggable: true}).addTo(this.map);
-        this.marker.bindPopup("侦察样机: " + this.id).openPopup();
-        // 绑定右键菜单显示事件
+        this.marker.bindPopup("飞机: " + this.id).openPopup();
 
-        const latLng = this.marker.getLatLng();
-        this.marker.bindPopup("侦察样机: " + this.id + "<br>经纬度: " + latLng.lat.toFixed(6) + ", " + latLng.lng.toFixed(6)).openPopup();
-
-        // 监听拖动过程中的事件
+        // 绑定拖动事件
         this.marker.on('drag', (event) => {
-            const latLng = event.target.getLatLng();  // 获取当前经纬度
-            this.marker.setPopupContent("侦察样机: " + this.id + "<br>经纬度: " + latLng.lat.toFixed(6) + ", " + latLng.lng.toFixed(6));
-            this.marker.openPopup();  // 更新并重新打开弹出窗口
+            const latLng = event.target.getLatLng();
+            this.marker.setPopupContent("飞机: " + this.id + "<br>经纬度: " + latLng.lat.toFixed(6) + ", " + latLng.lng.toFixed(6));
+            this.marker.openPopup();
+
+            // 更新 track 数组中的初始位置（如果有路径点，更新初始位置的坐标）
+            if (this.track.length > 0) {
+                this.setLat(latLng.lat);
+                this.setLng(latLng.lng);
+                this.track[0] = [latLng.lat.toFixed(5), latLng.lng.toFixed(5)];
+            }
+
+            // 更新初始 marker 的位置（即第一个路径点）
+            if (this.routeMarkers.length > 0) {
+                // 只更新初始位置标记，不更新后续标记
+                this.routeMarkers[0].setLatLng(latLng);
+            }
+
+            // 如果路径点数量大于等于2，更新前两个点之间的连线
+            if (this.routeMarkers.length >= 3) {
+                const firstMarkerLatLng = this.routeMarkers[0].getLatLng();
+                const secondMarkerLatLng = this.routeMarkers[2].getLatLng();
+                this.routeMarkers[1].remove();
+                this.routeMarkers[1] = L.polyline([firstMarkerLatLng, secondMarkerLatLng], {color: 'red'}).addTo(this.map);
+                console.log(this.routeMarkers);
+            }
         });
 
         this.marker.on('contextmenu', (event) => {
@@ -48,7 +74,11 @@ class Reconnaissance {
             this.contextMenu.show(event, this);
         });
 
-        axios.post("http://127.0.0.1:8081/api/reconnaissances/uploadCoordinates", {
+        const initialPosition = this.marker.getLatLng();
+        // 添加初始位置的红色标记
+        this.addRoutesMarker(initialPosition, "red");
+
+        axios.post("http://127.0.0.1:8081/api/airplanes/uploadCoordinates", {
             id: this.id,
             lat: this.marker.getLatLng().lat,
             lon: this.marker.getLatLng().lng
@@ -59,13 +89,13 @@ class Reconnaissance {
         });
     }
 
+
+
     setFlightRoute() {
-        this.routes = []; // 初始化路径数组
         console.log("请设置当前点迹");
 
         // 添加当前 marker 的位置到路径中
-        const initialPosition = this.marker.getLatLng();
-        axios.post("http://127.0.0.1:8081/api/reconnaissances/uploadCoordinates", {
+        axios.post("http://127.0.0.1:8081/api/airplanes/uploadCoordinates", {
             id: this.id,
             lat: this.marker.getLatLng().lat,
             lon: this.marker.getLatLng().lng
@@ -74,10 +104,6 @@ class Reconnaissance {
         }).catch(error => {
             console.error(`路径点 ${this.id} 坐标上传失败:`, error);
         });
-        this.routes.push([initialPosition.lat, initialPosition.lng]);
-
-        // 添加初始位置的红色标记
-        this.addRoutesMarker(initialPosition, "red");
 
         // 左键点击事件 - 添加点击的点到路线中
         const addRoutePoint = (event) => {
@@ -87,7 +113,7 @@ class Reconnaissance {
             }
 
             const latLng = event.latlng; // 获取点击位置的经纬度
-            axios.post("http://127.0.0.1:8081/api/reconnaissances/uploadCoordinates", {
+            axios.post("http://127.0.0.1:8081/api/airplanes/uploadCoordinates", {
                 id: this.id,
                 lat: this.marker.getLatLng().lat,
                 lon: this.marker.getLatLng().lng
@@ -96,16 +122,16 @@ class Reconnaissance {
             }).catch(error => {
                 console.error(`路径点 ${this.id} 坐标上传失败:`, error);
             });
-            this.routes.push([latLng.lat, latLng.lng]); // 添加到路径数组中
-            this.addRoutesMarker(latLng, "red"); // 标记点击位置为红色
+
             // 如果有上一个点，则绘制线段连接
-            if (this.routes.length > 1) {
-                const previousLatLng = this.routes[this.routes.length - 2];
+            if (this.track.length >= 1) {
+                const previousLatLng = this.track[this.track.length - 1];
                 const polyline = L.polyline([previousLatLng, [latLng.lat, latLng.lng]], {color: 'red'}).addTo(this.map);
                 this.routeMarkers.push(polyline); // 将线段添加到 routeMarkers 数组中}
                 console.log(`添加点迹: ${latLng.lat}, ${latLng.lng}`);
-
             }
+            this.addRoutesMarker(latLng, "red"); // 标记点击位置为红色
+            this.track.push([latLng.lat, latLng.lng]); // 添加到路径数组中
         };
 
         // 右键点击事件 - 结束路径设置
@@ -123,23 +149,8 @@ class Reconnaissance {
         this.map.on("contextmenu", endRouteSetting);
     }
 
-    // 添加标记点的辅助函数
-    addRoutesMarker(latLng, color) {
-        if (!latLng || isNaN(latLng.lat) || isNaN(latLng.lng)) {
-            console.error("无效的 LatLng 对象，无法添加标记。");
-            return;
-        }
-
-        const marker = L.circleMarker(latLng, {
-            color: color,
-            radius: 5
-        }).addTo(this.map);
-
-        this.routeMarkers.push(marker); // 将标记添加到 routeMarkers 数组中
-    }
-
     startFlight() {
-        if (this.routes.length < 2) {
+        if (this.track.length < 2) {
             console.log("路径点不足，无法移动");
             return;
         }
@@ -168,13 +179,13 @@ class Reconnaissance {
         if (!this.moving) {
             return;
         }
-        if (this.currentRouteIndex >= this.routes.length - 1) {
+        if (this.currentRouteIndex >= this.track.length - 1) {
             console.log("到达终点");
             this.moving = false;
             return;
         }
         const startPoint = this.marker.getLatLng();//开始点应该是什么呢？
-        const endPoint = L.latLng(this.routes[this.currentRouteIndex + 1]);
+        const endPoint = L.latLng(this.track[this.currentRouteIndex + 1]);
         const totalDistance = this.map.distance(startPoint, endPoint); // 计算总距离
         const updateInterval = 50; // 每步50ms
         let currentStep = 0;
@@ -207,17 +218,17 @@ class Reconnaissance {
         moveStep(); // 开始移动
     }
 
-    setSpeed() {
-        // 设置当前侦察样机的速度
-        this.speed = parseInt(prompt("初始速度300,请输入新的速度", this.speed));
-        this.marker.getPopup().setContent("侦察样机: " + this.speed);
-        axios.post("http://127.0.0.1:8081/api/reconnaissances/setSpeed", {
+    setItemSpeed() {
+        // 设置当前飞机的速度
+        this.setSpeed(parseInt(prompt("初始速度300,请输入新的速度", this.speed)));
+        this.marker.getPopup().setContent("飞机: " + this.speed);
+        axios.post("http://127.0.0.1:8081/api/airplanes/setSpeed", {
             id: this.id,
             speed: this.speed
         }).then(response => {
-            console.log(`侦察样机 ${this.id} 速度设置成功`);
+            console.log(`飞机 ${this.id} 速度设置成功`);
         }).catch(error => {
-            console.error(`侦察样机 ${this.id} 速度设置失败:`, error);
+            console.error(`飞机 ${this.id} 速度设置失败:`, error);
         });
     }
 
@@ -281,43 +292,22 @@ class Reconnaissance {
         });
     }
 
+
     deleteRoute() {
         this.stopFlight();
-        this.routes = [];
-        this.routeMarkers.forEach(marker => marker.remove());
-        this.routeMarkers = [];
-        const url = `http://127.0.0.1:8081/api/reconnaissances/deleteTrackPoint?uuid=${encodeURIComponent(this.id)}`;
+        this.track = [];
+        this.track.push([this.getLat(), this.getLng()]);
+        // 只删除路径标记，不删除初始坐标的标记
+        this.routeMarkers.slice(1).forEach(marker => marker.remove());  // 删除除初始坐标外的所有标记
+        this.routeMarkers = [this.routeMarkers[0]]; // 保留初始标记
+
+        const url = `http://127.0.0.1:8081/api/airplanes/deleteTrackPoint?uuid=${encodeURIComponent(this.id)}`;
         navigator.sendBeacon(url);
     }
 
-    deleteAllRoute() {
-        this.stopFlight();
-        this.routes = [];
-        this.routeMarkers.forEach(marker => marker.remove());
-        this.routeMarkers = [];
-        const url = `http://127.0.0.1:8081/api/reconnaissances/deleteTrackAllPoint}`;
-        navigator.sendBeacon(url);
-    }
-
-    getItemType(){
-        return this.itemType;
-    }
     getClassName(){
         return this.className;
     }
-    getName(){
-        return this.name;
-    }
 
-    getLat(){
-        return this.latitude;
-    }
 
-    getLng(){
-        return this.longitude;
-    }
-
-    getAlt(){
-        return this.altitude;
-    }
 }
