c11e9bd8d80fd953fa5a3a47905af55b81807a1c
guotuluo <2403105429@qq.com>
2024-12-17 18:17:51 +0800

    develop: 更新leaflet地图

--- js/mapController.js
+++ js/mapController.js
@@ -30,42 +30,40 @@ class MapController {
         //     })
         //     .catch(error => console.error("Error loading JSON:", error));
 
-        // 在线加载leaflet官方地图
-        // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        // }).addTo(map);
-
-        // // 使用高德的WMS服务
-        // var layer = L.tileLayer('http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
-        //     subdomains: ['1', '2', '3', '4'],
-        //     minZoom: 1, // 最小放缩级别
-        //     maxZoom: 10 // 最大放缩级别
-        // });
-        // map.addLayer(layer);
-
-
-        // 分片加载地图
-        L.tileLayer('tiles/{z}/{x}/{y}.png', {
-            maxZoom: 10,
-            minZoom: 0,
-            attribution: 'map'
+        // 在线加载leaflet官方地图 这个过一段时间就得去官网更新
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+
+        // 使用高德的WMS服务
+        var layer = L.tileLayer('http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
+            subdomains: ['1', '2', '3', '4'],
+            minZoom: 1, // 最小放缩级别
+            maxZoom: 10 // 最大放缩级别
+        });
+        map.addLayer(layer);
+
+
+        // // 分片加载地图
+        // L.tileLayer('tiles/{z}/{x}/{y}.png', {
+        //     maxZoom: 10,
+        //     minZoom: 0,
+        //     attribution: 'map'
+        // }).addTo(map);
         return map;
     }
 
-    addComponent(itemType, className, position){
+    addComponent(itemType, className, instanceName, position){
         const Icon = L.icon({
             iconUrl: 'images/' + itemType + '.png',
             iconSize: [32, 32],
             iconAnchor: [16, 16]
         });
-        const instanceName = itemType + String(componentManager.getNextInstanceName());
+
         const instance = ComponentFactory.createComponent(this.map, position, Icon, this.contextMenus[itemType], itemType, className, instanceName)
-
-
         // TODO 这里组件的组织需要想想,怎么管理所有组件
         // 每一次调用工厂创建新的对象都要将对象加入组件，删除的时候调用删除函数
-        componentManager.addInstance(itemType, className, instanceName, instance);
+        window.app.componentManager.addInstance(itemType, className, instanceName, instance);
         addObjectToList(instance);
 
         // 根据不同的类初始化特定菜单， 这里的itemType是一级菜单的类
@@ -84,11 +82,11 @@ class MapController {
             const dragData = e.dataTransfer.getData('text/plain');
 
             // 分割数据（假设数据是以'|'分隔的）
-            const [itemType, className, instanceName] = dragData.split('|');
-            console.log(itemType,className,instanceName);
+            const [itemType, className] = dragData.split('|');
             // 获取拖放位置的地理坐标
-            const latLng = this.map.mouseEventToLatLng(e);
-            this.addComponent(itemType, className, latLng);
+            const position = this.map.mouseEventToLatLng(e);
+            const instanceName = className + String(window.app.componentManager.getNextInstanceName());
+            this.addComponent(itemType, className, instanceName, position);
         });
     }
 }
