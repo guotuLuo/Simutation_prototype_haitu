2f849ccbcebb379425e4d23cb54f0cfd8e3b502d
guotuluo <2403105429@qq.com>
2024-12-17 18:17:28 +0800

    develop: 根据重构的各个大类进行整体逻辑重构，尽量解耦

--- js/main.js
+++ js/main.js
@@ -1,13 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const mapController = new MapController();
-    const sidebar = new Sidebar();
-    const toolbar = new Toolbar(mapController);
+// 定义全局管理器类 App
+class App {
+    constructor() {
+        window.app = this;
+        this.initApplication();
+    }
 
-    // mapController.contextMenus.initializeMenu(); // 确保菜单在页面加载后初始化
-    // 是否需要用mapController来管理所有的组件？
-    //const toolbar = new Toolbar();
-    componentManager.init();
-    window.addEventListener("beforeunload", () => {
-        componentManager.deleteAllInstances();
-    });
+    initApplication(){
+        this.initOpenDialog();
+        this.initMapController();
+        this.initComponentManager();
+
+        this.initSideBar();
+        this.initMenuBar();
+        this.initToolBar();
+        this.initEnvi();
+
+        // 页面卸载前清理所有对象
+        window.addEventListener("beforeunload", () => {
+            window.app.componentManager.deleteAllObjects();
+        });
+    }
+
+    initOpenDialog(){
+        this.openDialog = new OpenDialog();
+        this.openDialog.initializeOpenDialog();
+    }
+
+    initMapController(){
+        this.mapController = new MapController();
+    }
+
+    initComponentManager(){
+        this.componentManager = new componentManager();
+        this.componentManager.initComponentManager();
+    }
+
+    initSideBar(){
+        this.sidebar = new Sidebar();
+        this.sidebar.initializeSidebar();
+    }
+
+    initMenuBar(){
+        this.menubar = new Menubar();
+        this.menubar.initializeMenubar();
+
+    }
+
+    initToolBar(){
+        this.toolbar = new Toolbar();
+        this.toolbar.initializeToolbar();
+    }
+
+    initEnvi(){
+        this.envi = new Envi(this.mapController, this.componentManager);
+        this.envi.initEnvi();
+    }
+}
+
+// 在 DOMContentLoaded 事件中实例化 App
+document.addEventListener("DOMContentLoaded", () => {
+    window.app = new App();
 });
